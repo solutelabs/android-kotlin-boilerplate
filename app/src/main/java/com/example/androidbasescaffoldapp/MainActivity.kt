602d7d63e3b4c47252bffa27ce42be7670cbed71
Milind Mevada <milind.mevada@solutelabs.com>
2020-01-20 11:01:33 +0530

    Added coroutines based perimission methods

--- app/src/main/java/com/example/androidbasescaffoldapp/MainActivity.kt
+++ app/src/main/java/com/example/androidbasescaffoldapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.androidbasescaffoldapp
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.androidbasescaffoldapp.base.BaseActivity
 import com.example.androidbasescaffoldapp.remote_config.RemoteConfigRepository
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,5 +17,15 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textMain.text = RemoteConfigRepository()["welcome_msg"].asString()
+        lifecycleScope.launchWhenCreated {
+            val permission =
+                askPermission(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            permission?.let {
+                Log.d(
+                    "PERMISSION_RESULT",
+                    "${permission.grantResult[0] == PackageManager.PERMISSION_GRANTED}"
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/androidbasescaffoldapp/base/BaseActivity.kt
+++ app/src/main/java/com/example/androidbasescaffoldapp/base/BaseActivity.kt
@@ -4,9 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.androidbasescaffoldapp.analytics.getAnalyticsLogger
 import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Deferred
 
 @SuppressLint("Registered")
 abstract class BaseActivity : AppCompatActivity() {
@@ -26,7 +26,7 @@ abstract class BaseActivity : AppCompatActivity() {
         val data: Intent?
     )
 
-    var currentCode: Int = 0
+    private var currentCode: Int = 0
     private val resultByCode = mutableMapOf<Int, CompletableDeferred<ActivityResult?>>()
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -38,7 +38,7 @@ abstract class BaseActivity : AppCompatActivity() {
         }
     }
 
-    fun launchIntent(intent: Intent): Deferred<ActivityResult?> {
+    suspend fun launchIntent(intent: Intent): ActivityResult? {
         val activityResult = CompletableDeferred<ActivityResult?>()
 
         if (intent.resolveActivity(packageManager) != null) {
@@ -48,7 +48,55 @@ abstract class BaseActivity : AppCompatActivity() {
         } else {
             activityResult.complete(null)
         }
-        return activityResult
+        return activityResult.await()
     }
+
+    data class PermissionResult(
+        val permissions: Array<out String>,
+        val grantResult: IntArray
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as PermissionResult
+
+            if (!permissions.contentEquals(other.permissions)) return false
+            if (!grantResult.contentEquals(other.grantResult)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = permissions.contentHashCode()
+            result = 31 * result + grantResult.contentHashCode()
+            return result
+        }
+    }
+
+    private var permissionCode: Int = 0
+    private val permissionResultByCode = mutableMapOf<Int, CompletableDeferred<PermissionResult?>>()
+
+    suspend fun askPermission(permissions: Array<String>): PermissionResult? {
+        val permissionResult = CompletableDeferred<PermissionResult?>()
+        val resultCode = permissionCode++
+        permissionResultByCode[resultCode] = permissionResult
+        ActivityCompat.requestPermissions(this, permissions, resultCode)
+        return permissionResult.await()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        permissionResultByCode[requestCode]?.let {
+            it.complete(PermissionResult(permissions, grantResults))
+            permissionResultByCode.remove(requestCode)
+        } ?: run {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
 
 }
